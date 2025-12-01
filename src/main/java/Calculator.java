1c193531c69bca77486ed4e8abd4003a18208414
plykina <plykinaa13@yandex.ru>
2025-12-02 00:36:15 +0300

    realization Calculator and CalculatorTest

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,25 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a+b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a-b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            throw new ArithmeticException("Дедить на 0 нельзя!");
+        }
+        return a/b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a*b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        //((74 + 985) * 92 - 26) / 56
+        int sum = add(74, 985);
+        int product = times(sum, 92);
+        int difference = dif(product, 26);
+        int result = div(difference, 56);
+        return result;
     }
 }
