bea06da6ae45e34718ab330c56a2e108169bb20d
kolenka-matka <zlatkina.ekaterina@gmail.com>
2025-12-02 14:16:35 +0300

    resolved some problems

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,48 +1,32 @@
 public class Calculator {
-    public int add(int a, int b){
-        return a+b;
+    public int add(int a, int b) {
+        return a + b;
         //TODO inser your realisation in method add
     }
-    public int dif(int a, int b){
-        return a-b;
+
+    public int dif(int a, int b) {
+        return a - b;
         //TODO inser your realisation in method dif
     }
+
     public int div(int a, int b) throws Exception {
-        if (b == 0){
+        if (b == 0) {
             throw new Exception("decision by zero");
         }
-        return a/b;
+        return a / b;
         //TODO inser your realisation in method div
     }
-    public int times(int a, int b){
-        return a*b;
+
+    public int times(int a, int b) {
+        return a * b;
         //TODO inser your realisation in method times
     }
+
     public int solver() throws Exception {
         // уравнение (1000000000 - 23425) / 123 + 335*444 = ?
         int temp1 = dif(1000000000, 23425);
         int temp2 = div(temp1, 123);
-        return temp2  + times(335, 444);
+        return temp2 + times(335, 444);
         //TODO inser your realisation in method solver
-    }
-    public int dif(int a, int b){
-        return a-b;
-    }
-    public int div(int a, int b){
-        if (b == 0) {
-            throw new ArithmeticException("Дедить на 0 нельзя!");
-        }
-        return a/b;
-    }
-    public int times(int a, int b){
-        return a*b;
-    }
-    public int solver(){
-        //((74 + 985) * 92 - 26) / 56
-        int sum = add(74, 985);
-        int product = times(sum, 92);
-        int difference = dif(product, 26);
-        int result = div(difference, 56);
-        return result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,16 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class CalculatorTest {
     private final Calculator calculator = new Calculator();
+
     @Test
     void add() {
         assertEquals(15, calculator.add(10, 5));
         //TODO inser your realisation in test-method add
-    @Test
-    void add() {
-        assertEquals(10, calculator.add(10, 0));
-        assertEquals(25, calculator.add(15, 10));
     }
 
     @Test
@@ -24,14 +22,6 @@ class CalculatorTest {
         assertEquals(2, calculator.div(10, 5));
         assertThrows(Exception.class, () -> calculator.div(10, 0));
         //TODO inser your realisation in test-method div
-        assertEquals(10, calculator.dif(10, 0));
-        assertEquals(5, calculator.dif(15, 10));
-    }
-
-    @Test
-    void div() {
-        assertThrows(ArithmeticException.class, () -> calculator.div(10, 0));
-        assertEquals(1, calculator.div(10, 10));
     }
 
     @Test
@@ -46,15 +36,5 @@ class CalculatorTest {
         int result = calculator.solver();
         assertNotEquals(0, result);
         assertEquals(1, result, "fail test");
-        assertEquals(0, calculator.times(10, 0));
-        assertEquals(150, calculator.times(15, 10));
-    }
-
-    @Test
-    void solver() {
-        assertEquals(1059, calculator.add(74, 985));
-        assertEquals(97428, calculator.times(1059, 92));
-        assertEquals(97402, calculator.dif(97428, 26));
-        assertEquals(1739, calculator.div(97402, 56));
     }
 }
